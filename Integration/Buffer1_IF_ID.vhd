e27e555d00e96fd1d863a63960f0c5f51a31bb74
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2022-05-27 12:03:46 +0200

    IF/ID buffer

--- Integration/Buffer1_IF_ID.vhd
+++ Integration/Buffer1_IF_ID.vhd
@@ -2,7 +2,30 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY Buffer1_IF_ID IS
-    PORT ();
+    PORT (
+
+        clk : IN std_logic;
+        pc_input : IN std_logic_vector (19 DOWNTO 0);
+        inst_input : IN std_logic_vector (31 DOWNTO 0);
+        pc_output : OUT std_logic_vector (19 DOWNTO 0);
+        inst_output : OUT std_logic_vector (31 DOWNTO 0);
+ 
+    );
 END ENTITY;
 
 ARCHITECTURE a_Buffer1_IF_ID OF Buffer1_IF_ID IS
+
+BEGIN
+    process(clk)
+    IF falling_edge(clk) THEN
+        pc_output <= pc_input;
+        inst_output <= inst_input;
+
+    END IF;
+
+
+    end process;
+
+
+
+END ARCHITECTURE;
