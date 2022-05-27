fd974a8308993f439ffa471c89ab3d72a4d9a0ec
Mostafa <Moustafa.Achraf@hotmail.com>
2022-05-27 11:41:57 +0200

    Adding Buffer Files

---
+++ Integration/Buffer1_IF_ID.vhd
@@ -0,0 +1,8 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY Buffer1_IF_ID IS
+    PORT ();
+END ENTITY;
+
+ARCHITECTURE a_Buffer1_IF_ID OF Buffer1_IF_ID IS
---
+++ Integration/Integration.vhd
@@ -0,0 +1,11 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY Integration IS
+    PORT ();
+END ENTITY;
+ARCHITECTURE a_Integration OF Integration IS
+BEGIN
+
+END ARCHITECTURE;
