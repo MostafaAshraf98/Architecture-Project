e3757d166a3ae8016e77f4200a2bbc32b0847b79
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2022-05-27 12:26:35 +0200

    flush signal to IF/ID

--- Integration/Buffer1_IF_ID.vhd
+++ Integration/Buffer1_IF_ID.vhd
@@ -4,7 +4,7 @@ USE ieee.std_logic_1164.ALL;
 ENTITY Buffer1_IF_ID IS
     PORT (
 
-        clk, enb : IN std_logic;
+        clk, enb, flush : IN std_logic;
         pc_input : IN std_logic_vector (19 DOWNTO 0);
         inst_input : IN std_logic_vector (31 DOWNTO 0);
         pc_output : OUT std_logic_vector (19 DOWNTO 0);
@@ -20,7 +20,10 @@ BEGIN
     IF falling_edge(clk) and enb = '0' THEN
         pc_output <= pc_input;
         inst_output <= inst_input;
-
+    elsif flush = '1'  then
+        pc_output <= (others=>'0');
+        inst_output <= (others=>'0);
+        
     END IF;
 
 
